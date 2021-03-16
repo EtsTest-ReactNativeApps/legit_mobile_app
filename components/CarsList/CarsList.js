19d855a15e89e9080abcf65cb14d6c8c846717f4
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-16 12:01:57 -0700

    header

--- components/CarsList/CarsList.js
+++ components/CarsList/CarsList.js
@@ -13,6 +13,7 @@ const CarsList = (props) => {
                 data={cars}
                 renderItem={({item}) => <CarItem car={item} /> }
                 snapToAlignment={'start'}
+                showsVerticalScrollIndicator={false}
                 decelerationRate={'fast'}
                 snapToInterval={Dimensions.get('window').height}
                 />
