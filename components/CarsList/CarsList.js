9c1fb91e10e1b630acaac7a7eda35e0a7158b0e3
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-03-16 11:49:50 -0700

    rendering wiev animation fixed

--- components/CarsList/CarsList.js
+++ components/CarsList/CarsList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, FlatList } from 'react-native'
+import { View, FlatList, Dimensions } from 'react-native'
 import styles from './styles'
 import cars from './cars'
 import CarItem from '../CarItem/CarItem'
@@ -12,6 +12,9 @@ const CarsList = (props) => {
                 <FlatList 
                 data={cars}
                 renderItem={({item}) => <CarItem car={item} /> }
+                snapToAlignment={'start'}
+                decelerationRate={'fast'}
+                snapToInterval={Dimensions.get('window').height}
                 />
             </View>
         )
